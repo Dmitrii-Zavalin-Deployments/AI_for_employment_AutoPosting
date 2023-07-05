ebffe1ee34953c6503960dc3fab950c77959588d
Dmitrii <zavalin.dmitry@gmail.com>
2023-07-05 16:50:18 -0300

    Updated index.js and timer

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/*
 // Import the libraries
 const pantry = require('pantry-node');
 const { TwitterApi } = require('twitter-api-v2');
@@ -9,10 +8,10 @@ const pantryClient = new pantry(pantryID);
 
 // Create a new TwitterApi object with your app key, app secret, access token and access token secret from environment variables
 const client = new TwitterApi({
-  appKey: process.env.APP_KEY,
-  appSecret: process.env.APP_SECRET,
-  accessToken: process.env.ACCESS_TOKEN,
-  accessSecret: process.env.ACCESS_SECRET
+    appKey: process.env.APP_KEY,
+    appSecret: process.env.APP_SECRET,
+    accessToken: process.env.ACCESS_TOKEN,
+    accessSecret: process.env.ACCESS_SECRET
 });
 
 // Set the options for parsing JSON
@@ -20,11 +19,17 @@ const options = { parseJSON: true };
 
 main()
 async function main() {
-    const GetPosts = await GetPostsFunction();
-    const PostsArray = await Object.values(GetPosts)[0];
-    const ChosenPostGet = await ChosenPost(PostsArray.length);
-    const PostToPublish = PostsArray[ChosenPostGet];
-    await postTweet(PostToPublish);
+    const runNumber = process.env.GITHUB_RUN_NUMBER;
+    console.log("This is run number " + runNumber);
+    if (runNumber % 2 == 0) {
+        const GetPosts = await GetPostsFunction();
+        const PostsArray = await Object.values(GetPosts)[0];
+        const ChosenPostGet = await ChosenPost(PostsArray.length);
+        const PostToPublish = PostsArray[ChosenPostGet];
+        await PostTweet(PostToPublish);
+    } else {
+        await PostTweet("Discover AI for employment! Automatically searches for new jobs. Catches the most suitable ones. Prepares a resume, a cover letter and a link to apply. https://ai-for-employment.bitbucket.io/ #AIforEmployment #JobSearch #ApplyNow");
+    }
 }
 
 async function GetPostsFunction() {
@@ -43,16 +48,10 @@ async function ChosenPost(PostsArrayLength) {
     })
 }
 
-async function postTweet(data) {
+async function PostTweet(data) {
     // Use the v2 tweet method to post a tweet with text
     const tweet = await client.v2.tweet(data);
 
     // Log the tweet ID and URL
     console.log(tweet.data.id);
-}
-*/
-main()
-async function main() {
-  const runNumber = process.env.GITHUB_RUN_NUMBER;
-  console.log("This is run number " + runNumber);
 }
